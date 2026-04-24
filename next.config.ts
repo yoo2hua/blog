56d1d05d118e77525f3b2754dd0ad07b38135e05
totoro <woshiydh08@gmail.com>
2026-04-25 00:00:08 +0800

    fix: basePath and assetPrefix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // ✅ 关键
+  basePath: '/blog', // ⭐ 关键
+  assetPrefix: '/blog/', // ⭐ 关键
+
   images: {
     unoptimized: true, // ✅ 避免图片问题
   },
